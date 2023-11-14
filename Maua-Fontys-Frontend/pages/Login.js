894cbd4095091e89445069e9d4c3912d3f80e37d
Chantal M <c.maasspam@gmail.com>
2023-11-14 16:47:59 +0100

    login styling

--- Maua-Fontys-Frontend/pages/Login.js
+++ Maua-Fontys-Frontend/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, TouchableOpacity, StyleSheet, Image } from 'react-native';
+// import Style from '../'
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -18,21 +19,67 @@ const Login = () => {
   
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Welcome to PRINT</Text>
-        <TextInput
+        {/* Header */}
+      <View style={styles.header}>
+      </View>
+        {/* Body with Columns */}
+        <View style={styles.body}>
+          {/* Column 1 */}
+          <View style={styles.column}>
+            <Image
+              source={require('../assets/logo-maua.png')} 
+              style={styles.logo}
+            />
+          <Text style={styles.Title}>Welcome to PRINT</Text>
+            <Text style={styles.Title}>Welcome back</Text>
+            <Text style={styles.smalltext}>Please sign in</Text>
+            <TextInput
+              style={styles.input}
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              placeholder="Email@maua.br"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry
+              value={password}
+              onChangeText={setPassword}
+            />
+            <View style={styles.checkboxContainer}>
+              <TouchableOpacity
+                  onPress={() => setRememberMe(!rememberMe)}
+                  style={styles.checkbox}>
+                  {rememberMe ? (
+                  <Text style={styles.checkboxText}>x</Text>
+                  ) : null}
+              </TouchableOpacity>
+            <Text>Remember me</Text>
+            <TouchableOpacity onPress={handleForgotPassword}>
+              <Text style={styles.forgotPassword}>Forgot password?</Text>
+            </TouchableOpacity>
+            </View>
+              <Button title="Login" onPress={handleLogin} />
+          </View>
+        </View>
+      
+
+
+
+        {/* <TextInput
           style={styles.input}
           placeholder="Email@maua.br"
           value={email}
           onChangeText={setEmail}
-        />
-        <TextInput
+        /> */}
+        {/* <TextInput
           style={styles.input}
           placeholder="Password"
           secureTextEntry
           value={password}
           onChangeText={setPassword}
-        />
-        <View style={styles.checkboxContainer}>
+        /> */}
+        {/* <View style={styles.checkboxContainer}>
           <TouchableOpacity
             onPress={() => setRememberMe(!rememberMe)}
             style={styles.checkbox}>
@@ -46,51 +93,128 @@ const Login = () => {
           </TouchableOpacity>
         </View>
 
-        <Button title="Login" onPress={handleLogin} />
+        <Button title="Login" onPress={handleLogin} /> */}
       </View>
     );
   };
   
   const styles = StyleSheet.create({
+    // New:
     container: {
       flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 20,
+      flexDirection: 'column',
+      width: 400,
+      textAlign: 'center',
     },
-    title: {
-      fontSize: 24,
-      marginBottom: 20,
+    header: {
+      // height: 80,
+      // backgroundColor: 'blue',
+      // justifyContent: 'center',
+      // alignItems: 'center',
+      // width: 400,
+      // marginTop: 20,
+        width: 0,
+        height: 0,
+        backgroundColor: 'transparent',
+        borderStyle: 'solid',
+        borderRightWidth: 800,
+        borderTopWidth: 120,
+        borderLeftColor: 'transparent',
+        borderRightColor: 'transparent',
+        borderTopColor: '#095DAC',
+      
+    },
+    headerText: {
+      color: 'white',
+      fontSize: 18,
+    },
+    logo: {
+      width: 220, 
+      height: 100,
+      margin: 30, 
+    },
+    body: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      padding: 10,
+    },
+    column: {
+      flex: 1,
+      margin: 5,
+      justifyContent: 'center',
+      textAlign: 'left',
+      alignItems: 'center',
     },
     input: {
-      width: '100%',
+      width: '80%',
       height: 40,
       borderColor: 'gray',
       borderWidth: 1,
-      marginBottom: 20,
-      paddingHorizontal: 10,
+      marginTop: 10,
+      padding: 5,
     },
-    checkboxContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      marginBottom: 20,
-    },
-    checkbox: {
-      width: 20,
-      height: 20,
-      borderColor: 'gray',
-      borderWidth: 1,
+    footer: {
+      height: 50,
+      backgroundColor: 'blue',
       justifyContent: 'center',
       alignItems: 'center',
-      marginRight: 5
     },
-    checkboxText: {
-      fontSize: 16,
+    footerText: {
+      color: 'white',
+      fontSize: 18,
     },
-    forgotPassword: {
-      color: 'blue',
-      marginLeft: 10
+    Title:{
+      fontSize: 20,
+      color: '#095DAC',
+      fontWeight: 'bold',
+      textAlign: 'left', 
     },
+    smalltext:{
+      fontSize: 10,
+      textAlign: 'left', 
+    }
+    // Old:
+    // container: {
+    //   flex: 1,
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   padding: 20,
+    // },
+    // title: {
+    //   fontSize: 24,
+    //   marginBottom: 20,
+    // },
+    // input: {
+    //   width: '100%',
+    //   height: 40,
+    //   borderColor: 'gray',
+    //   borderWidth: 1,
+    //   marginBottom: 20,
+    //   paddingHorizontal: 10,
+    // },
+    // checkboxContainer: {
+    //   flexDirection: 'row',
+    //   alignItems: 'center',
+    //   marginBottom: 20,
+    // },
+    // checkbox: {
+    //   width: 20,
+    //   height: 20,
+    //   borderColor: 'gray',
+    //   borderWidth: 1,
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   marginRight: 5
+    // },
+    // checkboxText: {
+    //   fontSize: 16,
+    // },
+    // forgotPassword: {
+    //   color: 'blue',
+    //   marginLeft: 10
+    // },
   });
 
 export default Login;
+

c99164a15f259b08a7cb0656db30e71ecea2e9c6
martin.ropke <martin.ropke@gmail.com>
2023-12-13 20:50:13 -0300

    Update Constants

--- Maua-Fontys-Frontend/utils/Constants.js
+++ Maua-Fontys-Frontend/utils/Constants.js
@@ -1,10 +1,11 @@
-const baseAWSURL = "https://gts2ru7toi.execute-api.us-east-1.amazonaws.com/prod"
+const baseAWSURL = "https://8799vu1mu8.execute-api.us-east-1.amazonaws.com/prod"
 
 const Constants = {
     activities_url: `${baseAWSURL}/activities`,
     courses_url: `${baseAWSURL}/courses`,
     organizations_url: `${baseAWSURL}/organizations`,
     notigications_url: `${baseAWSURL}/notifications`,
+    registrations_url: `${baseAWSURL}/registrations`,
     mauaImage: 'https://f.i.uol.com.br/fotografia/2021/04/27/1619550291608860537182f_1619550291_16x9_lg.jpg',
     rectorImage: 'https://maua.br/img/upload/042020/marcelo-nitz-06103441.png',
     viceRectorImage: 'https://maua.br/img/infomaua/textos/foto1/297.jpg'

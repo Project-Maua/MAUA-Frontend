5482d29e41434d0248d9d5b4df6bab2ce07b28e7
Chantal M <c.maasspam@gmail.com>
2023-12-14 12:06:20 +0100

    Update MauaFontysInfoPage.js

--- Maua-Fontys-Frontend/pages/MoreInfoPages/MauaFontysInfoPage.js
+++ Maua-Fontys-Frontend/pages/MoreInfoPages/MauaFontysInfoPage.js
@@ -24,20 +24,20 @@ const MauaFontysInfoPage = ({route}) => {
                 <Text style={customStyles.card_title}>{t('What is the Maua-Fontys project?')}</Text>
                 <Text style={customStyles.card_message}>{"Students from Fontys in the Netherlands and students from Maua University in Brazil worked together to put together this application. The students who contributed to this project are:"}</Text>
                 <Text style={customStyles.card_title}>{"From Brazil:"}</Text>
-                <Text style={customStyles.card_message}>{"Raphael: "}</Text>
-                <Text style={customStyles.card_message}>{"Matheus: "}</Text>
-                <Text style={customStyles.card_message}>{"Gustavo: "}</Text>
-                <Text style={customStyles.card_message}>{"Martin: "}</Text>
+                <Text style={customStyles.card_message}>{"Raphael Marchetti Calciolari: "}</Text>
+                <Text style={customStyles.card_message}>{"Matheus Nakai: "}</Text>
+                <Text style={customStyles.card_message}>{"Gustavo Zamboni: "}</Text>
+                <Text style={customStyles.card_message}>{"Martin Ropke: "}</Text>
                 <Text style={customStyles.card_title}>{"From the Netherlands:"}</Text>
-                <Text style={customStyles.card_message}>{"Chantal: https://www.linkedin.com/in/chantal-maas-605aa22a4/"}</Text>
-                <Text style={customStyles.card_message}>{"Stijn: https://www.linkedin.com/in/stijn-hoeks-3764a4193?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"}</Text>
-                <Text style={customStyles.card_message}>{"Jarno: https://www.linkedin.com/in/jarno-looij-176b26180?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"}</Text>
-                <Text style={customStyles.card_message}>{"Tim: https://www.linkedin.com/in/tim-spitters?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"}</Text>
-                <Text style={customStyles.card_message}>{"Jason: "}</Text>
-                <Text style={customStyles.card_message}>{"Ben: "}</Text>
+                <Text style={customStyles.card_message}>{"Chantal Maas: https://www.linkedin.com/in/chantal-maas-605aa22a4/"}</Text>
+                <Text style={customStyles.card_message}>{"Stijn Hoeks: https://www.linkedin.com/in/stijn-hoeks-3764a4193?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"}</Text>
+                <Text style={customStyles.card_message}>{"Jarno Looij: https://www.linkedin.com/in/jarno-looij-176b26180?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"}</Text>
+                <Text style={customStyles.card_message}>{"Tim Spitters: https://www.linkedin.com/in/tim-spitters?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"}</Text>
+                <Text style={customStyles.card_message}>{"Jason Spaninks: "}</Text>
+                <Text style={customStyles.card_message}>{"Ben van der Linden: "}</Text>
                 <Text style={customStyles.card_title}>{"Teachers/Professors"}</Text>
-                <Text style={customStyles.card_message}>{"From the Netherlands: Mark"}</Text>
-                <Text style={customStyles.card_message}>{"From Brazil: Rodrigo"}</Text>
+                <Text style={customStyles.card_message}>{"From the Netherlands: Mark Mestrom"}</Text>
+                <Text style={customStyles.card_message}>{"From Brazil: Rodrigo Bossini"}</Text>
                 <Text style={customStyles.card_title}>{"More information"}</Text>
                 <Text style={customStyles.card_message}>{"For more information about the students and the schools, please check the links below"}</Text>
                 <Text style={customStyles.card_message}>{"https://maua.br/ "}</Text>
